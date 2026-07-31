42e87feaf7695980e7047e744fbe44a4710cbd3a
bohdantyshchenko-prog <bohdantyshchenko@gmail.com>
2026-07-31 15:32:48 +0300

    fix: import material context for family scope

--- lib/src/core/state/family_store.dart
+++ lib/src/core/state/family_store.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 
-import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class FamilyEntry {
